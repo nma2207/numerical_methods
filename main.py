b48cb464075f885593f60434a9f0711da9f959a0
Marat Nabiev <nma2207@gmail.com>
2017-02-26 15:14:30 +0300

    Tried to correct main.py, but I'm stupid

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def f(x, eps):
 
 def l(x_arr, x, k, n):
     result = 0.
-    for i in range(n):
+    for i in range(n+1):
         if i == k:
             break
         p1 = 1.0
@@ -30,6 +30,16 @@ def l(x_arr, x, k, n):
                 p2 *= float(x_arr[k] - x_arr[j])
         result += p1 / float((x - x_arr[k]) * (x - x_arr[i]) * p2)
     return result
+
+def l_1(x_arr, x, i, n):
+    result=1.0
+    for j in range(n+1):
+        if(j==i):
+            break
+        result*=float(x-x_arr[j])/float(x_arr[i]-x_arr[j])
+    return result
+
+
 
 
 def c(x):
@@ -43,7 +53,7 @@ def L(n, a, b, x):
         x_arr.append(a + h * i)
     result = 0.0
     for i in range(n):
-        result += f(x_arr[i], eps) * l(x_arr, x, i, n)
+        result += f(x_arr[i], eps) * l_1(x_sarr, x, i, n)
     return result
 
 
@@ -54,9 +64,9 @@ def main():
     n = 10
     h = float(b - a) / n
     for i in range(n):
-        x.append(a + h/2+ i * h)
+        x.append(a +h/2+ i * h)
     for i in x:
-        print 'x={0:.2f}       f={1:.5f}        L={2:.5f}      res={3:.5f}'.format(i,f(i,eps), L(n, a, b, i), c(i))
+        print 'x={0:.2f}       f={1:.5f}        L={2:.5f}      c={3:.5f}'.format(i,f(i,eps), L(n, a, b, i), c(i))
 
 
 if __name__ == "__main__":
