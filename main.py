a17e1774ea5f4a1aa521b1c728ca1af132aa3af1
Marat Nabiev <nma2207@gmail.com>
2017-03-01 23:08:15 +0300

    Corrected Lagrandj's polynom

--- main.py
+++ main.py
@@ -18,25 +18,26 @@ def f(x, eps):
 
 def l(x_arr, x, k, n):
     result = 0.
-    for i in range(n+1):
+    for i in range(n):
         if i == k:
-            break
+            continue
         p1 = 1.0
         for j in range(n):
             p1 *= float(x - x_arr[j])
         p2 = 1.
         for j in range(n):
-            if (j != k):
+            if (j!= k):
                 p2 *= float(x_arr[k] - x_arr[j])
         result += p1 / float((x - x_arr[k]) * (x - x_arr[i]) * p2)
     return result
 
 def l_1(x_arr, x, i, n):
     result=1.0
-    for j in range(n+1):
+    for j in range(n):
         if(j==i):
-            break
+            continue
         result*=float(x-x_arr[j])/float(x_arr[i]-x_arr[j])
+    #print 'result=',result
     return result
 
 
@@ -52,8 +53,8 @@ def L(n, a, b, x):
     for i in range(n + 1):
         x_arr.append(a + h * i)
     result = 0.0
-    for i in range(n):
-        result += f(x_arr[i], eps) * l_1(x_sarr, x, i, n)
+    for i in range(n+1):
+        result += f(x_arr[i], eps) * l(x_arr, x, i, n)
     return result
 
 
@@ -61,7 +62,7 @@ def main():
     x = []
     a = 0
     b = 1.5
-    n = 10
+    n = 100
     h = float(b - a) / n
     for i in range(n):
         x.append(a +h/2+ i * h)
